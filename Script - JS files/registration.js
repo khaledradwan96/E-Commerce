4a3c3ec72d90f0a3dff49c610a66c944fbff2b74
Khaled Radwan <khaledradwan96@gmail.com>
2025-05-24 18:26:54 +0300

    add some validation for registration

--- Script - JS files/registration.js
+++ Script - JS files/registration.js
@@ -2,9 +2,11 @@
 const registerForm = document.getElementById('registerForm');
 registerForm?.addEventListener('submit', (e) => {
     e.preventDefault();
+
     const fName = document.getElementById('fName').value;
     const lName = document.getElementById('lName').value;
     const name = `${fName} ${lName}`;
+    const userName = document.getElementById('userName').value;
     const birthDate = document.getElementById('birthDate').value;
     const gender = document.getElementById('birthDate').value;
     const email = document.getElementById('email').value;
@@ -12,38 +14,51 @@ registerForm?.addEventListener('submit', (e) => {
     const password = document.getElementById('password').value;
     const rePassword = document.getElementById('rePassword').value;
 
-
-    if (password !== rePassword) {
-        alert('Passwords do not match');
-        return;
-    }
-
     const userData = {
         name,
+        userName,
         birthDate,
         gender,
         email,
         phone,
         password
     };
-
+    
     let users = JSON.parse(localStorage.getItem('users')) || [];
-    users.push(userData);
-    localStorage.setItem('users', JSON.stringify(users));
-    window.location.href = 'login.html';
+    
+    // ===== Validation =====
+    // Check password
+    if (password !== rePassword) {
+        alert('Passwords do not match');
+        return;
+    }
+    
+    const foundUser = users.find( user => user.userName === userName);
+    const foundEmail = users.find( user => user.email === email);
+    if(foundUser){ // Check used userName
+        alert('user name is already used')
+    }else if(foundEmail){ // Check used email
+        alert('email is already used');
+    }else{ // save data
+        users.push(userData);
+        localStorage.setItem('users', JSON.stringify(users)); 
+        window.location.href = 'login.html';
+    }
 });
 
 // ============ login ============
 const loginForm = document.getElementById('loginForm');
 loginForm?.addEventListener('submit', (e) => {
     e.preventDefault();
+
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const errorMsg = document.getElementById('errorMsg');
 
     const users = JSON.parse(localStorage.getItem('users')) || [];
-    const foundUser = users.find( user => user.email === email);
 
+    // Check data
+    const foundUser = users.find( user => user.email === email);
     if (!foundUser) {
         errorMsg.textContent = 'User not found';
     } else if (foundUser.password !== password) {
